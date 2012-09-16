95d2e8933d49402e6a8f85a97886a8f6f2c5cf8d
Matthew Baxter-Reynolds (@mbrit) <mbrit@mbrit.com>
2012-09-16 01:40:21 -0500

    Fixed resolution of LoggingEnvironment

---
+++ MetroLog/LogWriteContext.cs
@@ -0,0 +1,31 @@
+﻿using MetroLog.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroLog
+{
+    public class LogWriteContext
+    {
+        private static ILoggingEnvironment _environment;
+
+        public LogWriteContext()
+        {
+        }
+
+        static LogWriteContext()
+        {
+            _environment = PlatformAdapter.Resolve<ILoggingEnvironment>();
+        }
+
+        public ILoggingEnvironment Environment
+        {
+            get
+            {
+                return _environment;
+            }
+        }
+    }
+}
