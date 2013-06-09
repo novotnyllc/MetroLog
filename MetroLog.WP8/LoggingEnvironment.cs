db815e6024b4c79e475953543d56fe10290aff49
ScottIsAFool <scottisafool@live.co.uk>
2013-06-09 16:21:10 +0100

    Added LogConfigurator/LoggingEnvironment

---
+++ MetroLog.WP8/LoggingEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using MetroLog.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroLog
+{
+    /// <summary>
+    /// Holds the logging environment for .NET apps.
+    /// </summary>
+    public class LoggingEnvironment : LoggingEnvironmentBase
+    {
+        public string MachineName { get; private set; }
+
+        public LoggingEnvironment()
+            : base(Environment.Version.ToString())
+        {
+            this.MachineName = "WP8Device";
+        }
+    }
+}
