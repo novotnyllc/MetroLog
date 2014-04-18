7e03f7784d1c1e891ed25ab0e337f6b1a09a89e3
Oren Novotny <oren@novotny.org>
2014-04-18 09:16:03 -0400

    Move linked files into shared project

---
+++ MetroLog.Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("Lightweight Logging Framework for Portable code")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2012 The MetroLog Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.6.0")]
+[assembly: AssemblyFileVersion("0.8.6.0")]
+
+[assembly: InternalsVisibleTo("MetroLog.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b4b7e56af036d" +
+                                                        "ed95a703d3ffc367837e047f6fbdba8fae1d1295a5c031a108aa452c62f14b7a12766f86a0f94f" +
+                                                        "28dae83980e5b69e97985899ae9299dd6951b0933d13416c564b3997d8ed784208afa2e18f0389" +
+                                                        "9da14d209054ce3002e47e9ae1bd811baa2a20899c5f6c8211b886be9284541d58476fbabc7323" +
+                                                        "90a0f7c9")]
+
+[assembly: InternalsVisibleTo("MetroLog.NetCore.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b4b7e56af036d" +
+                                                        "ed95a703d3ffc367837e047f6fbdba8fae1d1295a5c031a108aa452c62f14b7a12766f86a0f94f" +
+                                                        "28dae83980e5b69e97985899ae9299dd6951b0933d13416c564b3997d8ed784208afa2e18f0389" +
+                                                        "9da14d209054ce3002e47e9ae1bd811baa2a20899c5f6c8211b886be9284541d58476fbabc7323" +
+                                                        "90a0f7c9")]
+
+//[assembly: InternalsVisibleTo("MetroLog.NetFx.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b4b7e56af036d" +
+//                                                        "ed95a703d3ffc367837e047f6fbdba8fae1d1295a5c031a108aa452c62f14b7a12766f86a0f94f" +
+//                                                        "28dae83980e5b69e97985899ae9299dd6951b0933d13416c564b3997d8ed784208afa2e18f0389" +
+//                                                        "9da14d209054ce3002e47e9ae1bd811baa2a20899c5f6c8211b886be9284541d58476fbabc7323" +
+//                                                        "90a0f7c9")]
