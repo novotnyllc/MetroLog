cfc4b6e5663dd7a5a611b47e5db081a234f4cbdd
Oren Novotny <oren@novotny.org>
2015-02-10 10:56:55 -0500

    Add iOS unified support

---
+++ iOSUnifiedSample/RootViewController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using Foundation;
+using MetroLog;
+using UIKit;
+
+namespace iOSUnifiedSample
+{
+    public partial class RootViewController : UIViewController
+    {
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+
+        public RootViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+
+            // Initialize MetroLog using the defaults
+            LogManagerFactory.DefaultConfiguration.AddTarget(LogLevel.Trace, LogLevel.Fatal, new StreamingFileTarget());
+            ILogManager logManager = LogManagerFactory.DefaultLogManager;
+
+            // Inject the ILogManager manually
+            SomeMagicClass c = new SomeMagicClass(logManager);
+            c.DoMagic();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+
+    class SomeMagicClass
+    {
+        private readonly ILogger _log;
+        public SomeMagicClass(ILogManager logManager)
+        {
+            _log = logManager.GetLogger<SomeMagicClass>();
+        }
+
+        public void DoMagic()
+        {
+            // Log something interesting
+            _log.Info("Information - We are about to do magic!");
+            _log.Warn("Warning!");
+            _log.Trace("Trace some data.");
+            _log.Error("Something bad happened at {0}", DateTime.Now);
+            _log.Fatal("Danger Will Robinson!");
+
+            try
+            {
+                throw new InvalidOperationException("Test");
+            }
+            catch (Exception e)
+            {
+                _log.Error("Bad thing!", e);
+            }
+        }
+    }
+}
