1a32464d8efc814ccf5dfdcf541ac3b83e84c513
thomasgalliker <thomas@bluewin.ch>
2016-01-07 23:26:31 +0100

    Update project details (nuspec + AssemblyInfo)

--- Tests/MetroLog.Android.Tests/Properties/AssemblyInfo.cs
+++ Tests/MetroLog.Android.Tests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MetroLog.Android.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
